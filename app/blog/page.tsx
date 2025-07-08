43750fd55b4ade99907a0461b7e5c881d873eea0
Matías Insaurralde <matias@insaurral.de>
2025-07-08 19:35:18 -0300

    Update pages and sitemap
    Signed-off-by: Matías Insaurralde <matias@insaurral.de>

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,7 +2,7 @@ import { BlogPosts } from 'app/components/posts'
 
 export const metadata = {
   title: 'Blog',
-  description: 'Read my blog.',
+  description: 'Matias Insaurralde\s Blog',
 }
 
 export default function Page() {
